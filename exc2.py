a34121c21dc80f21bc4c8ec7dbef8cfe571c7d31
vojinKoprivica <76164930+vojinKoprivica@users.noreply.github.com>
2021-03-20 11:35:09 +0100

    novi zadaci

--- exc2.py
+++ exc2.py
@@ -4,3 +4,31 @@
 # Unesite broj:
 # U pitanju je:
 # Greska: neispravan unos podataka
+
+try:
+    broj = int(input("Unesti broj dana u nedelji: "))
+    if broj >= 1:
+    # broj = int(input("Unesti broj dana u nedelji: "))
+        dan = 0
+        if broj == 1:
+         dan = "Ponedeljak"
+        elif broj == 2:
+            dan = "Utorak"
+        elif broj == 3:
+            dan = "Sreda"
+        elif broj == 4:
+            dan = "Cetvrtak"
+        elif broj == 5:
+            dan = "Petak"
+        elif broj == 6:
+            dan = "Subota"
+        elif broj == 7:
+            dan = "Nedelja"
+        print(f"U pitanju je : {dan}")
+    else:
+        raise ValueError
+except ValueError:
+        print("Pogresan unos! Uneta vrednost mora biti broj veci od  0")
+
+
+
